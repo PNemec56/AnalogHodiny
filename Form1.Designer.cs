020dcd6b702d96753d93f83b806d04990aa0d3b9
Pnemec <hry.566@seznam.cz>
2023-03-01 14:39:25 +0100

    lepší ciferník

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,21 +36,23 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(106, 69);
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(354, 293);
+            this.pictureBox1.Size = new System.Drawing.Size(382, 306);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
--- Form1.cs
+++ Form1.cs
@@ -34,26 +34,57 @@ namespace AnalogHodiny
         {
             Graphics graphics = e.Graphics;
             Pen pen = new Pen(Color.Black, 3);
-            Rectangle rectangle = new Rectangle(10, 10, 150, 150);
+
+            // Získání velikosti PictureBox
+            int pictureBoxWidth = pictureBox1.Width;
+            int pictureBoxHeight = pictureBox1.Height;
+
+            // Nastavení velikosti a pozice obdélníku
+            int rectSize = Math.Min(pictureBoxWidth, pictureBoxHeight) - 40;
+            int rectX = (pictureBoxWidth - rectSize) / 2;
+            int rectY = (pictureBoxHeight - rectSize) / 2;
+            Rectangle rectangle = new Rectangle(rectX, rectY, rectSize, rectSize);
+
+            // Vykreslení ciferníku
             graphics.DrawEllipse(pen, rectangle);
 
             // Nastavení středu hodin
-            int centerX = rectangle.X + rectangle.Width / 2;
-            int centerY = rectangle.Y + rectangle.Height / 2;
+            int centerX = rectX + rectSize / 2;
+            int centerY = rectY + rectSize / 2;
 
-            // Vykreslení ciferníku
-            for (int i = 1; i <= 12; i++)
+            for (int i = 1; i <= 60; i++)
             {
-                double angle = (i * 30) * Math.PI / 180;
-                int x = (int)(centerX + (rectangle.Width / 2 - 25) * Math.Sin(angle));
-                int y = (int)(centerY - (rectangle.Height / 2 - 25) * Math.Cos(angle));
-                graphics.DrawString(i.ToString(), Font, Brushes.Black, new PointF(x, y));
+                double angle = (i * 6) * Math.PI / 180;
+                int x1 = (int)(centerX + (rectSize / 2 - 5) * Math.Sin(angle));
+                int y1 = (int)(centerY - (rectSize / 2 - 5) * Math.Cos(angle));
+                int x2 = (int)(centerX + (rectSize / 2 - (i % 5 == 0 ? 20 : 15)) * Math.Sin(angle));
+                int y2 = (int)(centerY - (rectSize / 2 - (i % 5 == 0 ? 20 : 15)) * Math.Cos(angle));
+
+                if (i % 5 == 0)
+                {
+                    // Každá pátá čárka bude delší a zvýrazněna
+                    graphics.DrawLine(new Pen(Brushes.Black, 3), new Point(x1, y1), new Point(x2, y2));
+                }
+                else
+                {
+                    // Ostatní čárky budou mít normální styl
+                    graphics.DrawLine(new Pen(Brushes.Black,0), new Point(x1, y1), new Point(x2, y2));
+                }
+
+                if (i % 5 == 0)
+                {
+                    int number = i / 5;
+                    double numberAngle = (i * 6) * Math.PI / 180;
+                    int numberX = (int)(centerX + (rectSize / 2 - 30) * Math.Sin(numberAngle));
+                    int numberY = (int)(centerY - (rectSize / 2 - 30) * Math.Cos(numberAngle));
+                    graphics.DrawString(number.ToString(), Font, Brushes.Black, new PointF(numberX, numberY));
+                }
             }
 
             // Nastavení délky ruček hodin
-            int secondHandLength = 70;
-            int minuteHandLength = 50;
-            int hourHandLength = 30;
+            int secondHandLength = rectSize / 2;
+            int minuteHandLength = rectSize / 3;
+            int hourHandLength = rectSize / 4;
 
             // Získání aktuálního času
             DateTime time = DateTime.Now;
@@ -75,6 +106,11 @@ namespace AnalogHodiny
             int hourHandX = (int)(centerX + hourHandLength * Math.Sin(hourAngle * (Math.PI / 180)));
             int hourHandY = (int)(centerY - hourHandLength * Math.Cos(hourAngle * (Math.PI / 180)));
             graphics.DrawLine(pen, centerX, centerY, hourHandX, hourHandY);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
